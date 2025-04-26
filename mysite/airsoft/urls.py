118799735d465d4871d7cde12a907e21dd1164f7
Katelin-Barbosa <112596593+Katelin-Barbosa@users.noreply.github.com>
2025-04-26 12:09:55 -0600

    Delete mysite directory

--- mysite/airsoft/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-app_name = "airsoft"  #namespacing to help browser find url of THIS app
-urlpatterns = [
-    path('', views.main_page, name="main_page")      
-]
